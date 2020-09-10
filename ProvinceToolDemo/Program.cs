16a153055b8f0395dbadace88e46b9834e523d2d
bren-wood <brendanwood@live.com>
2020-09-10 19:51:43 -0400

    Add project files.

---
+++ ProvinceToolDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvinceToolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a province code to get the value of the province.");
+
+                string provinceCode = Console.ReadLine();
+
+                if (ProvinceTool.validProvinceCode(provinceCode))
+                {
+                    string province = ProvinceTool.getProvinceName(provinceCode);
+                    Console.WriteLine(province);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the province entered was not recognized.");
+                }
+
+                Console.WriteLine("***");
+            }
+        }
+    }
+}
---
+++ ProvinceToolDemo/ProvinceTool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvinceToolDemo
+{
+    class ProvinceTool
+    {
+        static Dictionary<string, string> provinces = new Dictionary<string, string>();
+
+        private static void initialize()
+        {
+            provinces.Add("QC", "Québec");
+            provinces.Add("ON", "Ontario");
+            provinces.Add("BC", "British Columbia");
+        }
+
+        public static string getProvinceName(string provinceCode)
+        {
+            if (provinces.Count == 0)
+            {
+                initialize();
+            }
+
+            return provinces[provinceCode.Trim().ToUpper()];
+        }
+
+        public static bool validProvinceCode(string provinceCode)
+        {
+            if (provinces.Count == 0)
+            {
+                initialize();
+            }
+
+            return provinces.ContainsKey(provinceCode.Trim().ToUpper());
+        }
+
+    }
+}
